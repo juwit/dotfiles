0abb4663b7191ecf10bcf6a4b5254a9c413c2fdd
Julien WITTOUCK <julien.wittouck@gmail.com>
2020-01-07 17:19:01 +0100

    :sparkles: : add terraform_version variable mgmt

--- scripts/install-devops-tools.sh
+++ scripts/install-devops-tools.sh
@@ -4,8 +4,10 @@ set -e
 export GREEN="\e[32m"
 export NORMAL="\e[0m"
 
+TERRAFORM_VERSION="0.12.18"
+
 echo "${GREEN}installing terraform${NORMAL}"
-curl -o /tmp/terraform.zip https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip
+curl -o /tmp/terraform.zip https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 unzip -d /tmp /tmp/terraform.zip
-mv /tmp/terraform /opt
+sudo mv /tmp/terraform /opt
 rm /tmp/terraform.zip
