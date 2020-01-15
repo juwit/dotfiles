918d1762ce1e53b483d316a3073c3b6037d49b2f
Julien WITTOUCK <julien.wittouck@gmail.com>
2020-01-15 12:00:28 +0100

    :sparkles: : add docker-compose :whale: installation

--- scripts/install-devops-tools.sh
+++ scripts/install-devops-tools.sh
@@ -61,3 +61,9 @@ if [ ! -x "$(command -v docker)" ]; then
     sudo apt install -y docker-ce docker-ce-cli containerd.io
     sudo usermod -aG docker $USER
 fi
+
+if [ ! -x "$(command -v docker-compose)" ]; then
+    echo "${GREEN}installing docker-compose${NORMAL}"
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+fi
