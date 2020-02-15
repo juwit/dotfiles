afa06088ddcdee62f3101e72d04d29eb45143e0e
Julien WITTOUCK <julien.wittouck@gmail.com>
2020-02-15 12:29:18 +0100

    :recycle: : use binary distribution instead of package

--- scripts/install-golang.sh
+++ scripts/install-golang.sh
@@ -4,11 +4,12 @@ set -e
 export GREEN="\e[32m"
 export NORMAL="\e[0m"
 
-if [ ! -x "$(command -v go)" ]; then
+if [ ! -d /opt/go-1.13.8 ]; then
     echo "${GREEN}installing golang${NORMAL}"
-    sudo add-apt-repository ppa:longsleep/golang-backports
-    sudo apt-get update
-    sudo apt-get install -y golang-go
+    curl -Lo /tmp/go1.13.8.linux-amd64.tar.gz https://dl.google.com/go/go1.13.8.linux-amd64.tar.gz
+    sudo tar -C /opt -xf /tmp/go1.13.8.linux-amd64.tar.gz
+    sudo mv /opt/go /opt/go-1.13.8
+    rm /tmp/go1.13.8.linux-amd64.tar.gz
 fi
 
 if [ ! -x "$(command -v goland)" ]; then
