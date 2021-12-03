e709419ad94f08662ae12223362a97b6ec0db9cc
Julien WITTOUCK <julien.wittouck@gmail.com>
2021-12-03 09:48:44 +0100

    ✨ : add codium installation & alias

---
+++ scripts/install-coding.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+export GREEN="\e[32m"
+export NORMAL="\e[0m"
+
+echo "${GREEN}installing vscodium${NORMAL}"
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg 
+echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' | sudo tee /etc/apt/sources.list.d/vscodium.list
+sudo apt update && sudo apt install codium
--- zshrc
+++ zshrc
@@ -97,6 +97,7 @@ export EDITOR='vim'
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
+alias code=codium
 
 # direnv hook
 _direnv_hook() {
