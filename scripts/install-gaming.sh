e1ae2de46492c9b4f6a509a9b46c188d6f2fce0f
Julien WITTOUCK <julien.wittouck@gmail.com>
2021-12-03 09:41:59 +0100

    ✨ : add install-gaming script

---
+++ scripts/install-gaming.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export GREEN="\e[32m"
+export NORMAL="\e[0m"
+
+echo "${GREEN}installing steam${NORMAL}"
+sudo apt-get install -y steam
--- scripts/install-other.sh
+++ scripts/install-other.sh
@@ -6,8 +6,5 @@ export NORMAL="\e[0m"
 echo "${GREEN}installing spotify${NORMAL}"
 sudo snap install spotify
 
-echo "${GREEN}installing steam${NORMAL}"
-sudo apt-get install -y steam
-
 echo "${GREEN}installing discord${NORMAL}"
 sudo snap install discord
