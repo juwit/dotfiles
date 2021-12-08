6d30ecf47722619581ece96711a68f1682e6d50e
Julien WITTOUCK <julien.wittouck@gmail.com>
2021-12-08 14:23:46 +0100

    ✨ : add google-cloud-sdk install

--- scripts/install-devops-tools.sh
+++ scripts/install-devops-tools.sh
@@ -72,3 +72,11 @@ if [ ! -x "$(command -v docker-compose)" ]; then
     sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
 fi
+
+if [ ! -x "$(command -v gcloud)" ]; then
+    echo "${GREEN}installing google-cloud-sdk${NORMAL}"
+    sudo apt-get install apt-transport-https ca-certificates gnupg
+    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+    sudo apt-get update && sudo apt-get install -y google-cloud-sdk
+fi
