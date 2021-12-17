854b0521faf9d33d8f37cba2328561b8792cb284
Julien WITTOUCK <julien.wittouck@gmail.com>
2021-12-17 09:22:57 +0100

    ✨ : add command verification before install

--- scripts/install-node.sh
+++ scripts/install-node.sh
@@ -4,9 +4,18 @@ set -e
 export GREEN="\e[32m"
 export NORMAL="\e[0m"
 
-echo "${GREEN}installing nodejs${NORMAL}"
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt-get install -y nodejs
+if [ ! -x "$(command -v node)" ]; then
+    echo "${GREEN}installing nodejs${NORMAL}"
+    curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+    sudo apt-get install -y nodejs
+fi
 
-echo "${GREEN}installing gitmoji-changelog${NORMAL}"
-sudo npm install -g gitmoji-changelog
+if [ ! -x "$(command -v gitmoji-changelog)" ]; then
+    echo "${GREEN}installing gitmoji-changelog${NORMAL}"
+    sudo npm install -g gitmoji-changelog
+fi
+
+if [ ! -x "$(command -v zx)" ]; then
+    echo "${GREEN}installing zx${NORMAL}"
+    sudo npm install -g zx
+fi
