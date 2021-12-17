239e9c9483bce8a32b0ed6242e5c1a5f13f4a7cf
Julien WITTOUCK <julien.wittouck@gmail.com>
2021-12-17 09:33:59 +0100

    ♻️ : simplify dotfiles install

--- scripts/install-dotfiles.sh
+++ scripts/install-dotfiles.sh
@@ -4,26 +4,11 @@ set -e
 export GREEN="\e[32m"
 export NORMAL="\e[0m"
 
-cd ..
-
-DOTFILESDIR=$(pwd -P)
-
 echo "${GREEN}installing dot files${NORMAL}"
-
-for DOTFILE in *; do
-    HOMEFILE="$HOME/.$DOTFILE"
-    [ -d $DOTFILE ] && DOTFILE="$DOTFILE/"
-    DIRFILE="$DOTFILESDIR/$DOTFILE"
-
-    # ignoring script dir and .md file
-    echo $DOTFILE | egrep -q '(^scripts/$|\.md$)' && continue
-
-    # ignoring .sh files
-    echo $DOTFILE | egrep -q '\.sh$' && continue
-  
-    ln -sfv "$DIRFILE" "$HOMEFILE"
-   
-done
+ln -sfv $PWD/vimrc ~/.vimrc
+ln -sfv $PWD/zshrc ~/.zshrc
+ln -sfv $PWD/gitconfig ~/.gitconfig
+mkdir -p ~/.config/git && ln -sfv $PWD/gitignore ~/.config/git/ignore
 
 echo "${GREEN}installing oh-my-zsh for the root user${NORMAL}"
 sudo ln -sfv "$DOTFILESDIR/zshrc" /root/.zshrc
