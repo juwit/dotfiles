af210e2d7822b354033af6526cad87976c9d0269
Julien WITTOUCK <julien.wittouck@gmail.com>
2021-12-22 17:18:13 +0100

    ✨ : update java install

--- scripts/install-java.sh
+++ scripts/install-java.sh
@@ -4,16 +4,18 @@ set -e
 export GREEN="\e[32m"
 export NORMAL="\e[0m"
 
-if [ ! -d /opt/jdk-11.0.6+10 ]; then
-    echo "${GREEN}installing openjdk 11${NORMAL}"
-    curl -Lo /tmp/openjdk11.0.6_10.tar.gz https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.6%2B10/OpenJDK11U-jdk_x64_linux_hotspot_11.0.6_10.tar.gz
-    sudo tar -C /opt -xf /tmp/openjdk11.0.6_10.tar.gz
-    rm /tmp/openjdk11.0.6_10.tar.gz
+JAVA_VERSION="17.0.1+12"
+if [ ! -d /opt/jdk-${JAVA_VERSION} ]; then
+    echo "${GREEN}installing jdk ${JAVA_VERSION} ${NORMAL}"
+    curl -Lo /tmp/jdk-${JAVA_VERSION}.tar.gz https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.1%2B12/OpenJDK17U-jdk_x64_linux_hotspot_17.0.1_12.tar.gz
+    sudo tar -C /opt -xf /tmp/jdk-${JAVA_VERSION}.tar.gz
+    rm /tmp/jdk-${JAVA_VERSION}.tar.gz
 fi
 
-if [ ! -d /opt/apache-maven-3.6.3 ]; then
+MAVEN_VERSION="3.8.4"
+if [ ! -d /opt/apache-maven-${MAVEN_VERSION} ]; then
     echo "${GREEN}installing maven 3${NORMAL}"
-    curl -Lo /tmp/apache-maven-3.6.3-bin.tar.gz http://apache.crihan.fr/dist/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
-    sudo tar -C /opt -xf /tmp/apache-maven-3.6.3-bin.tar.gz
-    rm /tmp/apache-maven-3.6.3-bin.tar.gz
+    curl -Lo /tmp/apache-maven-${MAVEN_VERSION}-bin.tar.gz http://apache.crihan.fr/dist/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz
+    sudo tar -C /opt -xf /tmp/apache-maven-${MAVEN_VERSION}-bin.tar.gz
+    rm /tmp/apache-maven-${MAVEN_VERSION}-bin.tar.gz
 fi
