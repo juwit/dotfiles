1e9975d5355a79bb027661e9f342dd5b277c0914
Julien WITTOUCK <julien.wittouck@gmail.com>
2021-12-29 14:37:34 +0100

    ✨ : add vlc install

--- scripts/install-media-gaming.sh
+++ scripts/install-media-gaming.sh
@@ -3,7 +3,10 @@
 source utils.sh
 
 print "installing steam"
-sudo apt-get install -y steam
+sudo apt install -y steam
 
 print "installing spotify"
 flatpak install -y flathub com.spotify.Client
+
+print "installing vlc"
+sudo apt install -y vlc
