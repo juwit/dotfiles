23baf1a1d88d94c490a729338c2dab5d090a0ea2
Julien WITTOUCK <julien.wittouck@gmail.com>
2022-01-03 15:12:44 +0100

    🐛 : add -y option to helm install

--- scripts/install-devops-tools.sh
+++ scripts/install-devops-tools.sh
@@ -45,7 +45,7 @@ if [ ! -x "$(command -v helm)" ]; then
     sudo apt-get install apt-transport-https --yes
     echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
     sudo apt-get update
-    sudo apt-get install helm
+    sudo apt-get install -y helm
 fi
 
 if [ ! -f /opt/kubectx ]; then
