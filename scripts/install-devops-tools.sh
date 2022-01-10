504ca718b95fd48f92a9d0bd3d5c68478a27d4d5
Julien WITTOUCK <julien.wittouck@gmail.com>
2022-01-10 17:59:15 +0100

    🐛 : correct kubectl install

--- scripts/install-devops-tools.sh
+++ scripts/install-devops-tools.sh
@@ -31,7 +31,7 @@ if [ ! -x "$(command -v packer)" ]; then
     sudo apt-get update && sudo apt-get install packer
 fi
 
-if [ ! -f /opt/kubectl ]; then
+if [ ! -x "$(command -v kubectl)" ]; then
     print "installing kubectl"
     sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
     echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
