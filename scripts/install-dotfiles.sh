1c7edf498e3c5305c55ed6f8150f005d4ad8e6b0
Julien WITTOUCK <julien.wittouck@gmail.com>
2022-09-12 10:22:33 +0200

    ✨ : add direnvrc dotfile

--- scripts/install-dotfiles.sh
+++ scripts/install-dotfiles.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
 
-source utils.sh
+source scripts/utils.sh
 
 print "installing dot files"
 ln -sfv $PWD/vimrc ~/.vimrc
 ln -sfv $PWD/zshrc ~/.zshrc
 ln -sfv $PWD/gitconfig ~/.gitconfig
 mkdir -p ~/.config/git && ln -sfv $PWD/gitignore ~/.config/git/ignore
+mkdir -p ~/.config/direnv && ln -svf $PWD/direnvrc ~/.config/direnv/direnvrc
 
 print "installing oh-my-zsh for the root user"
 sudo ln -sfv "$DOTFILESDIR/zshrc" /root/.zshrc
