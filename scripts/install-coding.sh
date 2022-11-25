3043ba4dcbe826cb08e1f9e88e2e35e11b6a616a
Julien WITTOUCK <julien.wittouck@gmail.com>
2022-11-25 11:49:39 +0100

    ✨ : add gh install

--- scripts/install-coding.sh
+++ scripts/install-coding.sh
@@ -28,3 +28,11 @@ if [ ! -f /usr/local/bin/difft ]; then
     tar xvf /tmp/difft.tar.gz
     sudo mv difft /usr/local/bin/
 fi
+
+if [ ! -x "$(command -v gh)" ]; then
+    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+    && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+    && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+    && sudo apt update \
+    && sudo apt install gh -y
+fi
