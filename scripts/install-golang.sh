93f920297e6ff245d56a8d6200058f02f2e3dc1a
Julien WITTOUCK <julien.wittouck@gmail.com>
2024-05-28 17:06:26 +0200

    ⬆️ : update golang 1.22.3

--- scripts/install-golang.sh
+++ scripts/install-golang.sh
@@ -3,11 +3,13 @@ set -e
 
 source utils.sh
 
-GOLANG_VERSION=1.17.5
+GOLANG_VERSION=1.22.3
 
-if [ ! -d /opt/go-$GOLANG_VERSION ]; then
-    print "installing golang"
-    curl -Lo /tmp/go$GOLANG_VERSION.linux-amd64.tar.gz https://dl.google.com/go/go$GOLANG_VERSION.linux-amd64.tar.gz
-    sudo tar -C /opt -xf /tmp/go$GOLANG_VERSION.linux-amd64.tar.gz
-    rm /tmp/go$GOLANG_VERSION.linux-amd64.tar.gz
+if [ ! -d /opt/go/go-$GOLANG_VERSION ]; then
+    print "installing GoLang version $GOLANG_VERSION"
+    curl -Lo /tmp/go-$GOLANG_VERSION.linux-amd64.tar.gz https://dl.google.com/go/go$GOLANG_VERSION.linux-amd64.tar.gz
+    tar -C /tmp -xf /tmp/go-$GOLANG_VERSION.linux-amd64.tar.gz
+    sudo mkdir -p /opt/go
+    sudo mv /tmp/go /opt/go/go-$GOLANG_VERSION
+    rm /tmp/go-$GOLANG_VERSION.linux-amd64.tar.gz
 fi
