680fc3f83591bda2381722ff76ae70cb5b0b0b83
Julien WITTOUCK <julien.wittouck@gmail.com>
2024-05-29 08:06:54 +0200

    ✨ : use gum in script install

--- scripts/install-golang.sh
+++ scripts/install-golang.sh
@@ -6,10 +6,11 @@ source utils.sh
 GOLANG_VERSION=1.22.3
 
 if [ ! -d /opt/go/go-$GOLANG_VERSION ]; then
-    print "installing GoLang version $GOLANG_VERSION"
-    curl -Lo /tmp/go-$GOLANG_VERSION.linux-amd64.tar.gz https://dl.google.com/go/go$GOLANG_VERSION.linux-amd64.tar.gz
+    print "Installing GoLang version $GOLANG_VERSION"
+    gum spin --title "Downloading GoLang version $GOLANG_VERSION" -- curl -Lo /tmp/go-$GOLANG_VERSION.linux-amd64.tar.gz https://dl.google.com/go/go$GOLANG_VERSION.linux-amd64.tar.gz
     tar -C /tmp -xf /tmp/go-$GOLANG_VERSION.linux-amd64.tar.gz
     sudo mkdir -p /opt/go
     sudo mv /tmp/go /opt/go/go-$GOLANG_VERSION
     rm /tmp/go-$GOLANG_VERSION.linux-amd64.tar.gz
+    print "Done Installing GoLang version $GOLANG_VERSION"
 fi
