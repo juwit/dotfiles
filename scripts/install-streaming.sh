cdec63fa452295d723032b3116861dda3bf8ea65
Julien WITTOUCK <julien.wittouck@gmail.com>
2024-10-11 17:53:53 +0200

    ♻️ : update obs-install for arch

--- scripts/install-streaming.sh
+++ scripts/install-streaming.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-source utils.sh
+. ./detect.sh
 
-print "installing obs + plugin"
-flatpak install -y flathub com.obsproject.Studio
-flatpak install -y flathub com.obsproject.Studio.Plugin.WebSocket
+if [ $INSTALL_TOOL == "pacman" ]; then
+    sudo pacman -S --needed --noconfirm -q obs-studio
+else
+    print "installing obs + plugin"
+    flatpak install -y flathub com.obsproject.Studio
+    flatpak install -y flathub com.obsproject.Studio.Plugin.WebSocket
 
-print "installing webcam loopback plugin"
-sudo apt install -y v4l2loopback-dkms
+    print "installing webcam loopback plugin"
+    sudo apt install -y v4l2loopback-dkms
+
+fi
